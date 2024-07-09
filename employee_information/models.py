0d09c15a55a1878b3f51a1f5a79cb9da511d7fdb
Kikvila123 <gogoladzeg9090@gmail.com>
2024-07-09 07:32:12 -0700

    fINAL

--- employee_information/models.py
+++ employee_information/models.py
@@ -37,22 +37,32 @@ class Position(models.Model):
 
 
 class Employees(models.Model):
-    code = models.CharField(max_length=100,blank=True) 
-    firstname = models.TextField() 
-    middlename = models.TextField(blank=True,null= True) 
-    lastname = models.TextField() 
-    gender = models.TextField(blank=True,null= True) 
-    dob = models.DateField(blank=True,null= True) 
-    contact = models.TextField() 
-    address = models.TextField() 
-    email = models.TextField() 
-    department_id = models.ForeignKey(Department, on_delete=models.CASCADE) 
-    position_id = models.ForeignKey(Position, on_delete=models.CASCADE) 
-    date_hired = models.DateField() 
-    salary = models.FloatField(default=0) 
-    status = models.IntegerField() 
-    date_added = models.DateTimeField(default=timezone.now) 
+    code = models.CharField(max_length=100, blank=True)
+    firstname = models.TextField()
+    middlename = models.TextField(blank=True, null=True)
+    lastname = models.TextField()
+    gender = models.TextField(blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
+    contact = models.TextField()
+    address = models.TextField()
+    email = models.TextField()
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    position_id = models.ForeignKey(Position, on_delete=models.CASCADE)
+    date_hired = models.DateField()
+    salary = models.FloatField(default=0)
+    status = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(auto_now=True) 
+    
+def __str__(self):
+        firstname = self.firstname if self.firstname else ''
+        middlename = self.middlename if self.middlename else ''
+        lastname = self.lastname if self.lastname else ''
+        return f'{firstname} {middlename} {lastname}'
+    
 
-    def __str__(self):
-        return self.firstname + ' ' +self.middlename + ' '+self.lastname + ' '
+    
+
+
+
+    
